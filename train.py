f84e99e572a6a1dc675b19f2a09b6c84c8b1adb2
pantelis-giankulidis <67536962+pantelis-giankulidis@users.noreply.github.com>
2021-03-11 20:21:37 +0200

    validation function updated

--- train.py
+++ train.py
@@ -5,60 +5,49 @@ decoder_optimizer = torch.optim.Adam(params=filter(lambda p: p.requires_grad, de
 
 print_freq = 2
 
-def train(data_loader,encoder,decoder,embedding,max_caption_length,epochs=1):
+def train(data_loader,encoder,decoder,embedding,max_caption_length,decoder_optimizer):
 
     decoder_optimizer = torch.optim.Adam(params=filter(lambda p: p.requires_grad, decoder.parameters()), lr=0.01)
     encoder.train() #Train mode for CNN
     decoder.train() #Train mode for LSTM
     embedding.train()
 
-    for e in range(epochs):
-        for i, (image,captions,caption_lengths) in enumerate(data_loader):
+    for i, (image,captions,caption_lengths) in enumerate(data_loader):
+        images = encoder(image)
+        emb= embedding(captions) #(N,max_caption_length,embedding_dim)
 
-            images = encoder(image)
-            emb= embedding(captions) #(N,max_caption_length,embedding_dim)
+        images = images.view(images.size(0),images.size(1),images.size(2)*images.size(3))
 
-            images = images.view(images.size(0),images.size(1),images.size(2)*images.size(3))
+        scores,stochastic_loss =decoder(images,emb,caption_lengths,num_pixels=256)
 
-            scores,stochastic_loss =decoder(images,emb,caption_lengths,num_pixels=256)
+        targets = captions[:, 1:]#targets are from the first prediction
 
-            targets = captions[:, 1:]#targets are from the first prediction
+        loss = nn.CrossEntropyLoss(scores, targets)
+        loss+=stochastic_loss
 
-            loss = nn.CrossEntropyLoss(scores, targets)
-            loss+=stochastic_loss
-
-        	# Add doubly stochastic attention regularization
-            #loss += alpha_c * ((1. - alphas.sum(dim=1)) ** 2).mean()
-
-            # Back prop.
-            decoder_optimizer.zero_grad()
+        # Back prop.
+        decoder_optimizer.zero_grad()
             
-            loss.backward()
+        loss.backward()
 
-        	# Update weights
-            decoder_optimizer.step()
+        # Update weights
+        decoder_optimizer.step()
             
 
-            # Print status
-            if i % print_freq == 0:
-                print('Epoch ',i,' loss: %.2f'.format(loss))
-            if i==2:
-                break
+        # Print status
+        if i % print_freq == 0:
+            print('Epoch ',i,' loss: %.2f'.format(loss))
+        if i==2:
+            break
 
                 
-def validate(val_loader, encoder, decoder, criterion):
-    """
-    Performs one epoch's validation.
-    :param val_loader: DataLoader for validation data.
-    :param encoder: encoder model
-    :param decoder: decoder model
-    :param criterion: loss layer
-    :return: BLEU-4 score
-    """
+def validate(val_loader, encoder, decoder, embedding, max_caption_length):
+    
     decoder.eval()  # eval mode (no dropout or batchnorm)
-    if encoder is not None:
-        encoder.eval()
-
+    encoder.eval()
+    embedding.eval()
+    
+    
     batch_time = AverageMeter()
     losses = AverageMeter()
     top5accs = AverageMeter()
